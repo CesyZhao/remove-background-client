3c954e37b644618dfd37c18b80958fefc5c58113
ChrisZhao 赵思思 <997010638@qq.com>
2024-03-05 09:19:00 +0800

    feat: 环境检测。

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import EnvChecker from './components/EnvChecker.vue'
+import FileUploader from './components/FileUploader.vue'
+import { ref } from 'vue'
 
-
+const envReady = ref(false)
 </script>
 
 <template>
-  <EnvChecker />
+  <EnvChecker v-if="!envReady" @env-ready="envReady = true" />
+  <FileUploader v-else />
 </template>
---
+++ src/renderer/src/components/FileUploader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  点击上传文件
+</template>
+
+<style scoped>
+
+</style>
