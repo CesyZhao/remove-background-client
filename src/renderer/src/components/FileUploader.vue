3f153a8ace123c12cbb7f24424702a7f7150502a
ChrisZhao 赵思思 <997010638@qq.com>
2024-03-26 18:50:23 +0800

    feat: 环境检测。

--- src/renderer/src/components/FileUploader.vue
+++ src/renderer/src/components/FileUploader.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-  点击上传文件
+  <div class="file-uploader">
+    上传一张图片以消除背景
+    <div class="file-uploader-button">
+      上传图片
+    </div>
+    <div class="file-uploader-tip">
+      或者拖放一个文件、粘贴图片或 URL。
+    </div>
+  </div>
 </template>
 
 <style scoped>
