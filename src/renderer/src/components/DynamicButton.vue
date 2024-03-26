0156f961dbd11856a6ee5b647cf39b96e0af211f
ChrisZhao 赵思思 <997010638@qq.com>
2024-03-26 22:51:41 +0800

    feat: 文件上传器。

---
+++ src/renderer/src/components/DynamicButton.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue'
+
+const speedDeg = 0.01;
+const r = 0.5;
+
+const dom = ref()
+
+const frameId = ref(0)
+
+const rotateBg = () => {
+  if (dom.value) {
+    // eslint-disable-next-line react/no-find-dom-node
+    const nativeNode = dom.value as HTMLButtonElement;
+    if (nativeNode) {
+      const bgPos = nativeNode.style.backgroundPosition;
+      if (!bgPos) {
+        nativeNode.style.backgroundPosition = "0% 50%";
+      } else {
+        const arr = bgPos.split(" ");
+        let x = Number(arr[0].substring(0, arr[0].length - 1)) / 100 - r;
+        const oldX = x;
+        let y = Number(arr[1].substring(0, arr[1].length - 1)) / 100 - r;
+
+        x = x * Math.cos(speedDeg) - y * Math.sin(speedDeg);
+        y = y * Math.cos(speedDeg) + x * Math.sin(speedDeg);
+
+        nativeNode.style.backgroundPosition = `${(x + r) * 100}% ${
+          (y + r) * 100
+        }%`;
+      }
+
+      frameId.value = requestAnimationFrame(rotateBg);
+    }
+  }
+}
+
+onMounted(() => {
+  console.log(dom, '===========')
+  if (dom.value) {
+    console.log('++++++--------')
+    frameId.value = requestAnimationFrame(rotateBg);
+  }
+})
+
+onBeforeUnmount(() => {
+  if (frameId.value) {
+    cancelAnimationFrame(frameId.value);
+  }
+})
+</script>
+
+<template>
+  <div ref="dom" class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="less">
+.container {
+  padding: 13px 24px;
+  background-size: 400% 1000%;
+  cursor: pointer;
+  background-image: radial-gradient(
+    closest-side,
+    hsl(331, 70%, 46%) 6%,
+    hsl(331, 90%, 56%) 50%,
+    hsl(206, 75%, 49%) 100%
+  );
+  background-position: 0% 50%;
+  color: #ffffff;
+  margin-right: 24px;
+  box-shadow: 0 10px 20px -10px #35a2fd
+}
+
+</style>
--- src/renderer/src/components/FileUploader.vue
+++ src/renderer/src/components/FileUploader.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
 
+import DynamicButton from './DynamicButton.vue'
 </script>
 
 <template>
   <div class="file-uploader">
     上传一张图片以消除背景
-    <div class="file-uploader-button">
-      上传图片
-    </div>
+<!--    <div class="file-uploader-button">-->
+<!--      -->
+<!--    </div>-->
+    <dynamic-button class="file-uploader-button">上传图片</dynamic-button>
     <div class="file-uploader-tip">
       或者拖放一个文件、粘贴图片或 URL。
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="less" scoped>
+  .file-uploader {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 20px;
+    &-button {
+      padding: 8px 24px;
+      font-size: 16px;
+      margin: 24px;
+      border-radius: 32px;
+      cursor: pointer;
+    }
+    &-tip {
+      font-size: 12px;
+    }
+  }
 </style>
