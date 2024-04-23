a5b2aad835471f2c445e23d182dc390ffc6f5fd6
cesyzhao <997010638@qq.com>
2024-04-23 22:50:55 +0800

    feat: 环境检查

--- src/main/Bridge.ts
+++ src/main/Bridge.ts
@@ -24,7 +24,7 @@ class Bridge {
       try {
         const pythonInstalled = await checkPythonInstallStatus()
         this.webContents.send('env-check-reply', { status: pythonInstalled })
-        this.webContents.send('env-check-reply',{ status: EnvStatus.RembgIsInstalling })
+        this.webContents.send('env-check-reply', { status: EnvStatus.RembgIsInstalling })
         const result = await installRemBG('rembg[cli]')
         this.webContents.send('env-check-reply', { status: result })
       } catch (e) {
@@ -51,59 +51,53 @@ class Bridge {
   setupPythonDownload() {
     const isMac = process.platform === 'darwin'
     const baseURL = 'https://www.python.org/ftp/python/3.10.10'
+    const requestPath = `${baseURL}/${isMac ? 'python-3.10.10-macos11.pkg' : 'python-3.10.10-amd64.exe'}`
     ipcMain.on('install-python', async () => {
       this.webContents.send('env-check-reply', { status: EnvStatus.PythonDownloading })
-      https.get(
-        `${baseURL}/${isMac ? 'python-3.10.10-macos11.pkg' : 'python-3.10.10-amd64.exe'}`,
-        (res) => {
-          console.log(res.statusCode, '++++++')
-          if (res.statusCode !== '200') {
-            const file = fs.createWriteStream(
-              path.join(__dirname) +
-                `${res.req.path.split('/')[res.req.path.split('/').length - 1]}`
-            )
-            // 进度
-            // const len = parseInt(res.headers['content-length']) // 文件总长度
-            // console.log(len)
-            // let cur = 0
-            // res.on('data', function (chunk) {
-            //   cur += chunk.length
-            //   const progress = ((100.0 * cur) / len).toFixed(2) // 当前进度
-            //   // const currProgress = (cur / 1048576).toFixed(2) // 当前了多少
-            //   this.webContents.send('env-check-reply', progress)
-            //   //这里开启新的线程启动子窗子 将进度条数据传送至子窗口 显示下载进度。
-            //   // console.log(progress);
-            //   // console.log(currProgress + "M");
-            // })
-            res.on('end', (e) => {
-              console.log('下载结束')
-              //下载完成执行exe文件
-              // ToolsUpgrade(
-              //   path.join(__dirname) +
-              //     `${res.req.path.split('/')[res.req.path.split('/').length - 1]}`
-              // )
+      https.get(requestPath, (res) => {
+        console.log(res.statusCode, '++++++')
+        if (res.statusCode === 200) {
+          const filePaths = requestPath.split('/')
+          const file = fs.createWriteStream(
+            path.join(__dirname) + `${filePaths[filePaths.length - 1]}`
+          )
+          // 进度
+          // const len = parseInt(res.headers['content-length']) // 文件总长度
+          // console.log(len)
+          // let cur = 0
+          // res.on('data', function (chunk) {
+          //   cur += chunk.length
+          //   const progress = ((100.0 * cur) / len).toFixed(2) // 当前进度
+          //   // const currProgress = (cur / 1048576).toFixed(2) // 当前了多少
+          //   this.webContents.send('env-check-reply', progress)
+          //   //这里开启新的线程启动子窗子 将进度条数据传送至子窗口 显示下载进度。
+          //   // console.log(progress);
+          //   // console.log(currProgress + "M");
+          // })
+          res.on('end', (e) => {
+            console.log('下载结束')
+            //下载完成执行exe文件
+            // ToolsUpgrade(
+            //   path.join(__dirname) +
+            //     `${res.req.path.split('/')[res.req.path.split('/').length - 1]}`
+            // )
+          })
+          file
+            .on('finish', () => {
+              file.close()
             })
-            file
-              .on('finish', () => {
-                // console.log('文件写入结束')
-                file.close()
-              })
-              .on('error', (err) => {
-                fs.unlink(
-                  path.join(__dirname) +
-                    `${res.req.path.split('/')[res.req.path.split('/').length - 1]}`
-                )
-                if (err) {
-                  console.log(err)
-                }
-              })
-            res.pipe(file)
-          } else {
-            console.log('网络错误!')
-          }
-          // https://blog.csdn.net/qq_42172829/article/details/122090748
+            .on('error', (err) => {
+              fs.unlink(path.join(__dirname) + `${filePaths[filePaths.length - 1]}`, () => {})
+              if (err) {
+                console.log(err)
+              }
+            })
+          res.pipe(file)
+        } else {
+          console.log('网络错误!')
         }
-      )
+        // https://blog.csdn.net/qq_42172829/article/details/122090748
+      })
     })
   }
 }
